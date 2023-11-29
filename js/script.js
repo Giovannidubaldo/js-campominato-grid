0fbbf9d069a446c86b89f69bf02f0696ccf1b6dd
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2023-11-29 15:58:03 +0100

    Added numbers to cells

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ function createCells(num){
     let cell = document.createElement('div');
     cell.classList.add('square')
 
+    // Punto 4 readme.md
+    cell.innerHTML = num;
+
     return cell;
 }
 
@@ -10,8 +13,6 @@ let grid = document.getElementById('grid');
 
 // Punto 3 readme.md
 for (let i=0; i<100; i++){
-    let square = createCells();
-
-    
+    let square = createCells(i+1);
     grid.appendChild(square);
 }
